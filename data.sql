96037c1df4bbb27e531f716e484ab627f2d57b2f
syarotta1997 <helenlu1997@gmail.com>
2017-12-01 02:59:44 -0500

    1

--- data.sql
+++ data.sql
@@ -3,7 +3,7 @@ insert into questions values( 782,
                                            'MC',
                                            'To pledge your loyalty to the Sovereign, Queen Elizabeth II');
 insert into MC_incorrect_answers values(782, 'To pledge your allegiance to the flag and fulfill the duties of a Canadian','Think regally.');
-insert into MC_incorrect_answers values(782, 'To pledge your loyalty to Canada from sea to sea',null);
+insert into MC_incorrect_answers values(782, 'To pledge your loyalty to Canada from sea to sea','');
 
 insert into questions values( 566, 
                                            'The Prime Minister, Justin Trudeau, is Canada''s Head of State.?',
@@ -20,7 +20,7 @@ insert into NUM_incorrect_answers values(601, 1800,1900, 'The Quiet Revolution h
 insert into NUM_incorrect_answers values(601, 2000,2010, 'The Quiet Revolution happened some time ago.');
 insert into NUM_incorrect_answers values(601, 2020,3000, 'The Quiet Revolution has already happened!');
 
-insert into questions values( 782, 
+insert into questions values( 625, 
                                            'What is the Underground Railroad?',
                                            'MC',
                                            'A network used by slaves who escaped the United States into Canada');
@@ -56,7 +56,7 @@ insert into classes values(2, 1, 8, 120, 0998801234);
 insert into classes values(3, 1, 8, 120, 0010784522);
 insert into classes values(4, 1, 8, 120, 0997733991);
 insert into classes values(5, 1, 8, 120, 5555555555);
-insert into classes values(6, 2, 5, 366, 2222222222)
+insert into classes values(6, 2, 5, 366, 2222222222);
 
 insert into quiz values('Pr1-220310', 'Citizenship Test Practise Questions', True, '2017-10-01 13:30:00');
 
