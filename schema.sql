84ceefb63664b18c99e8492ebe82e9959b2b4480
syarotta1997 <helenlu1997@gmail.com>
2017-12-01 03:05:12 -0500

    1

--- schema.sql
+++ schema.sql
@@ -104,7 +104,6 @@ id int primary key,
 qid varchar(255) not null references quiz(qid),
 cid int not null references classes(id),
 question_id int not null references weight(id),
-unique(qid,cid)
 );
 
 CREATE TABLE response(
