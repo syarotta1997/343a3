489a2d54acd75843ea7f7736bf5a6d5cef9a7d27
syarotta1997 <helenlu1997@gmail.com>
2017-11-30 21:54:00 -0500

    1

--- schema.sql
+++ schema.sql
@@ -1,3 +1,33 @@
+/* 
+1. What constraints from the domain could not be enforced?
+
+    I) Only a student in the class that was assigned a quiz can answer questions on that quiz.
+        This constraint could not ever be enforced because it requires references of attrbutes 
+        across different relations, which check does not support such operations in SQL.  
+
+2. What constraints that could have been enforced were not enforced? Why not?
+    I) Numeric questions can only have integer answers
+
+           This constraint could be enforced by introducing sub-categories to answers
+           such as create another table NUM_ANSWER with its text field restricted to 
+           int type. However I chose not to do so because it will bring a great cost in having
+           redundent information, also makes inserting data more complex for others  
+    II)  A multiple choice question has answer options and there are always at least two.
+     
+         This constraint could also be enforced by splitting questions into sub-questions
+         relations. But I did not do so with the same reason stated in II). With multiple relations
+         relating to questions, inserting/defining Quiz, QuizBank and incorrect relations will be
+         very diffcult and will also include redundent informations which we want to avoid.
+         In addition, the feature of multiple choise has answer options is implemented within the
+         relation Answers, as user can simply insert a new answer to a question with unique aid and
+         answer text. The only problem is that there could be less than two answer options.
+
+    III) For numeric questions, each hint is specific to a given range of values
+
+        This constraint also deals with attribute formats, which can be resolved by introducing
+        sub tables with context specific attribute types. However I did not implement this constraint 
+        with the same reason in I).
+ */
 drop schema if exists quizschema cascade;
 create schema quizschema;
 set search_path to quizschema;
