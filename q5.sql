d8e6d5d627782e86c4d6e1510961f0c2d9c28641
syarotta1997 <helenlu1997@gmail.com>
2017-12-01 21:12:18 -0500

    1

---
+++ q5.sql
@@ -0,0 +1,53 @@
+/*
+Q5. For each question on quiz Pr1-220310, report the number of students in the grade 8 class in room 120 with
+Mr Higgins who got the question right, the number who got it wrong, and the number who did not answer it.
+
+*/
+set search_path to quizschema;
+
+DROP VIEW IF EXISTS class_higgins CASCADE;
+DROP VIEW IF EXISTS student_response CASCADE;
+DROP VIEW IF EXISTS correct CASCADE;
+DROP VIEW IF EXISTS incorrect CASCADE;
+DROP VIEW IF EXISTS no_ans CASCADE;
+-- first we get the class_member id of all students from specified class
+create view class_higgins as
+select id,sid
+from classes join room on classes.room = room.rid
+where room.rid = 120 and room.teacher = 'Mr Higgins' and classes.grade = 8;
+-- collect data of all selected student reaponses from the quiz by joining revelant tables
+create view student_response as
+select sid, weight.question_id, weight.weight, response.answer
+from quiz join quiz_assigned on quiz.qid = quiz_assigned.qid
+                join weight on quiz_assigned.wid = weight.id
+                join class_higgins on quiz_assigned.cid = class_higgins.id
+                join response on response.id = quiz_assigned.id
+where quiz.qid = 'Pr1-220310';
+-- create table and count total weights as total grade for students who have answer at least one question correctly
+create view correct as
+select sid, q.question_id
+from student_response as sr left join questions as q on sr.question_id = q.question_id
+where sr.answer = q.correct_ans;
+
+create view incorrect as
+select sid, q.question_id
+from student_response as sr left join questions as q on sr.question_id = q.question_id
+where sr.answer <> q.correct_ans and sr.answer <> '';
+-- from the schema we assume that all students in the same class have been assigned the same quiz, then
+-- for those who happen to have no response (missed the quiz or just did not answer at all)
+-- or 0 correctly answered questions, simply given them 0 as mark
+create view no_ans as
+select sid, q.question_id
+from student_response as sr left join questions as q on sr.question_id = q.question_id
+where sr.answer = '';
+
+select count(correct.sid) as num_correct, count(incorrect.sid) as num_incorrect, count(no_ans.sid) as num_no_answer
+from correct full join incorrect on question_id full join no_ans on question_id
+group by question_id;
+
+
+
+
+
+    
+
