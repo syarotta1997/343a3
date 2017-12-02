23355ee06318e41ca5951fb83acd8e3cfe74cde3
syarotta1997 <helenlu1997@gmail.com>
2017-12-01 19:52:38 -0500

    1

--- q2.sql
+++ q2.sql
@@ -15,10 +15,10 @@ where questions.q_type = 'TF';
 
 create view hints as
 select other_hints.question_id as question_ID, substring(other_hints.text,1,50) as text, count(other_hints.hint) as hint_count
-from ( select question_id, text,hint from questions as q  join MC_incorrect_answers as m 
+from ( select question_id, q.text,hint from questions as q  join MC_incorrect_answers as m 
                                                                                                    on q.question_id = m.quest_id
           union 
-          select question_id, text,hint from questions as q  join NUM_incorrect_answers as n 
+          select question_id, q.text,hint from questions as q  join NUM_incorrect_answers as n 
                                                                                                    on q.question_id = n.quest_id) 
           as other_hints
 group by other_hints.question_id, substring(other_hints.text,1,50);
