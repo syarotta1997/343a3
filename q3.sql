c0f34f122849068977069c270615e3d88f005ccd
syarotta1997 <helenlu1997@gmail.com>
2017-12-01 20:19:57 -0500

    1

--- q3.sql
+++ q3.sql
@@ -5,17 +5,30 @@ Mr Higgins. Report the student number, last name, and total grade.
 set search_path to quizschema;
 
 DROP VIEW IF EXISTS class_higgins CASCADE;
+DROP VIEW IF EXISTS student_response CASCADE;
+DROP VIEW IF EXISTS scores CASCADE;
 
 create view class_higgins as
 select id,sid
 from classes join room on classes.room = room.rid
 where room.rid = 120 and room.teacher = 'Mr Higgins' and classes.grade = 8;
 
+create view student_response as
 select sid, weight.question_id, weight.weight, response.answer
 from quiz join quiz_assigned on quiz.qid = quiz_assigned.qid
                 join weight on quiz_assigned.wid = weight.id
                 join class_higgins on quiz_assigned.cid = class_higgins.id
                 join response on response.id = quiz_assigned.id;
+
+create view scores as
+select sid, sum(sr.weight)
+from student_response as sr join questions as q on sr.question_id = q.question_id
+where sr.answer = q.correct_ans
+group by sid;
+
+select student.sid as student_number, student.last_name as last_name, scores.weight as total_grade
+from scores join student on scores.sid = student.sid;
+
 
 
 
