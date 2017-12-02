2936da3f5992adf24ef929f7d466389feeff0fa0
syarotta1997 <helenlu1997@gmail.com>
2017-12-01 19:14:37 -0500

    1

--- q1.sql
+++ q1.sql
@@ -3,7 +3,7 @@ Q1. Report the full name and student number of all students in the database.
 */
 set search_path to quizschema;
 
-select first_name || last_name as student_name, sid as student_number
+select first_name || ' ' || last_name as student_name, sid as student_number
 from student;
 
 
