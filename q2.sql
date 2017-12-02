208027682cc4b0fd0cb259aa3b888f2516589ee7
syarotta1997 <helenlu1997@gmail.com>
2017-12-01 19:39:28 -0500

    1

--- q2.sql
+++ q2.sql
@@ -24,7 +24,7 @@ from ( select question_id, hint from questions as q  join MC_incorrect_answers a
 group by other_hints.question_id;
 
 select *
-from TF_hint union hints;
+from (select * from TF_hint union hints) as all_hints;
 
 
 
